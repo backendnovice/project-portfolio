0ee13cc6013bc43b95978b634351fc0f947a8353
backendnovice <backendnovice@gmail.com>
2023-03-26 05:17:03 +0900

    :tada: 스프링 부트 프로젝트 생성

---
+++ src/test/java/backendnovice/projectportfolio/ProjectPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package backendnovice.projectportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPortfolioApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
