7a0ad02073aa7c07a287190f330a00edd5398023
backendnovice <backendnovice@gmail.com>
2023-03-28 20:33:30 +0900

    :sparkles: 생성일자, 수정일자를 담당하는 공통 도메인 추가

--- src/main/java/backendnovice/projectportfolio/ProjectPortfolioApplication.java
+++ src/main/java/backendnovice/projectportfolio/ProjectPortfolioApplication.java
@@ -2,7 +2,9 @@ package backendnovice.projectportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProjectPortfolioApplication {
     
